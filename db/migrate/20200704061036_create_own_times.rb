af3883fdb258fdee4e0109fbe126324ab611f3ab
sakozo <youlianggit@gmail.com>
2020-07-04 15:48:43 +0900

    マイグレーションファイル修正

--- db/migrate/20200704061036_create_own_times.rb
+++ db/migrate/20200704061036_create_own_times.rb
@@ -4,8 +4,8 @@ class CreateOwnTimes < ActiveRecord::Migration[5.2]
       t.string :name,          null: false                    # 何をする時間か
       t.references :user,      null: false, foreign_key: true # 作成者
       t.integer :type,         null: false                    # 時間マトリックスの領域
-      t.shareFlg :integer      null: false                    # 共有設定（共有 or プライベート）
-      t.publicLevel :integer     null: false                  # 公開範囲（プライベートのみ設定可能）
+      t.shareFlg :integer,     null: false                    # 共有設定（共有 or プライベート）
+      t.publicLevel :integer,    null: false                  # 公開範囲（プライベートのみ設定可能）
       
       t.timestamps
     end
