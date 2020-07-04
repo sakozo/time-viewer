bb6b439602ab3592f9ac95ff8d49e3abc2ff31cc
sakozo <youlianggit@gmail.com>
2020-07-04 18:46:40 +0900

    time_typeの登録機能実装

--- app/controllers/own_times_controller.rb
+++ app/controllers/own_times_controller.rb
@@ -1,2 +1,17 @@
 class OwnTimesController < ApplicationController
+  def create
+    own_time = OwnTime.new(own_time_params)
+    result = own_time.save
+
+    if result
+      redirect_to user_path(current_user.id)
+    else
+      # TODO:処理
+    end
+  end
+
+  private
+  def own_time_params
+    params.require(:own_time).permit(:name, :time_type, :shareFlg, :publicLevel).merge(user_id: current_user.id)
+  end
 end
