07efd8d0bd8ea63fff56c835a0456cf4d0a314a2
sakozo <youlianggit@gmail.com>
2020-07-04 15:46:34 +0900

    result_timeテーブルの定義

--- db/migrate/20200704070000_create_result_times.rb
+++ db/migrate/20200704070000_create_result_times.rb
@@ -1,7 +1,10 @@
 class CreateResultTimes < ActiveRecord::Migration[5.2]
   def change
     create_table :result_times do |t|
-
+      t.references :user,      null: false, foreign_key: true # 登録者
+      t.references :own_time,  null: false, foreign_key: true # 時間
+      t.integer :block,        null: false # ブロックid  ブロック1(5:00) ブロック2(5:15)...
+      
       t.timestamps
     end
   end
