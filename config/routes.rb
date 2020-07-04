d0d34c95bcc0788bccc3071b0910e0cb0ea48491
sakozo <youlianggit@gmail.com>
2020-07-04 21:30:35 +0900

    プロジェクトの登録

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :own_times, only: [:create]
   resources :result_times, only: [:create]
+  resources :projects, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
