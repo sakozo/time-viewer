1b5f6d7b4dc4f546dab23f5d153a9aa9cd354ca1
sakozo <youlianggit@gmail.com>
2020-07-04 15:28:17 +0900

    own_timeテーブル

---
+++ db/migrate/20200704061036_create_own_times.rb
@@ -0,0 +1,13 @@
+class CreateOwnTimes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :own_times do |t|
+      t.string :name,          null: false                    # 何をする時間か
+      t.references :user,      null: false, foreign_key: true # 作成者
+      t.integer :type,         null: false                    # 時間マトリックスの領域
+      t.shareFlg :integer      null: false                    # 共有設定（共有 or プライベート）
+      t.publicLevel :integer     null: false                  # 公開範囲（プライベートのみ設定可能）
+      
+      t.timestamps
+    end
+  end
+end
