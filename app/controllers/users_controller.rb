90801282950a7fe1f074936ed08513052409e53e
sakozo <youlianggit@gmail.com>
2020-07-04 20:57:25 +0900

    result_timeテーブルに登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
     @own_time = OwnTime.new
+    @result_time = ResultTime.new
 
     # TODO:天気自動記録機能の実装
     #環境変数をJSに渡す
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :own_times, only: [:create]
+  resources :result_times, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
