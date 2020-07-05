8431eabdb99dd3a5e83bf227fcfbf79eccbd087d
sakozo <youlianggit@gmail.com>
2020-07-05 23:23:43 +0900

    時間の入力・保存

--- app/controllers/result_times_controller.rb
+++ app/controllers/result_times_controller.rb
@@ -1,17 +1,30 @@
 class ResultTimesController < ApplicationController
   def create
-    # binding.pry
-    result_time = ResultTime.new(result_time_params)
+    error_flg = false
 
-    if result_time.save
-      redirect_to user_path(current_user.id)
-    else
-      # TODO:処理
+    block_params.each do |block|
+      data = result_time_params.merge(block: block[0]).merge(own_time_id: block[1])
+      #data = data.merge(own_time_id: block[1])
+      result_time = ResultTime.new(data)
+      unless result_time.save
+        error_flg = true
+      end
     end
+
+    if error_flg
+      # TODO:処理
+    else
+      redirect_to user_path(current_user.id)
+    end
+
   end
 
   private
   def result_time_params
-    params.require(:result_time).permit(:own_time_id, :block, :record_date).merge(user_id: current_user.id)
+    params.require(:result_time).permit(:record_date).merge(user_id: current_user.id)
+  end
+  
+  def block_params
+    params.require(:block)[0]
   end
 end
