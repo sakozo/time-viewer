c530987511b02bd62a2d9b29443fa8a8ec5dbed3
sakozo <youlianggit@gmail.com>
2020-07-11 15:22:35 +0900

    0埋め処理を追加

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -1,3 +1,13 @@
+// 1桁の数字を0埋めで2桁にする
+var toDoubleDigits = function(num) {
+  num += "";
+  if (num.length === 1) {
+    num = "0" + num;
+  }
+ return num;     
+};
+
+
 $(function() {
   var day = new Date();
   var year = day.getFullYear();
@@ -7,5 +17,5 @@ $(function() {
   var dayOfWeekStr = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ][dayOfWeek];
 
   var str = year + '/' + month + '/' + date + '(' + dayOfWeekStr + ')';
-  $('#record_date').val(year.toString() + month.toString() + date.toString());
+  $('#record_date').val(year.toString() + toDoubleDigits(month).toString() + toDoubleDigits(date).toString());
 });
