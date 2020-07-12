c74f62cd8f5a8b296b43ad2c0a8a7e24f81b6443
sakozo <youlianggit@gmail.com>
2020-07-12 15:45:16 +0900

    セレクトボックスに選択肢を渡す雛形作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
     @own_time = OwnTime.new
     @result_time = ResultTime.new
     @project = Project.new
+
+    # own_timeの選択肢を設定
+    @all_select_time = OwnTime.all
+    @select_time1 = OwnTime.where(id: 1)
+    @select_time2 = OwnTime.where(id: 1)
+    @select_time3 = OwnTime.where(id: 1)
+    @select_time4 = OwnTime.where(id: 1)
+
     # TODO:天気自動記録機能の実装
     #環境変数をJSに渡す
     #gon.wheather_api_key = ENV['OPEN_WEATHER_API_KEY']
