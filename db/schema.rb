0a79a301c31eb62d72c4fd1c9aafec273f60a867
sakozo <youlianggit@gmail.com>
2020-07-19 23:16:27 +0900

    add:完了日を入れる

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_015039) do
+ActiveRecord::Schema.define(version: 2020_07_19_140948) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_015039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "done_flg", null: false
+    t.date "done_date"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
