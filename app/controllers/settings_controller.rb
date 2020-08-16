59661d8bf67055bf887cdf015b2f915bdaf735cd
sakozo <youlianggit@gmail.com>
2020-08-16 16:41:17 +0900

    add: ユーザーごとに時間色を設定する機能

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,5 +8,24 @@ class SettingsController < ApplicationController
     @select_time2 = OwnTime.where(time_type: 2)
     @select_time3 = OwnTime.where(time_type: 3)
     @select_time4 = OwnTime.where(time_type: 4)
+
+    # colorここから
+    if UserColor.find_by(user_id: current_user.id).present?
+      # 色が登録済みの場合
+      @user_color = UserColor.find_by(user_id: current_user.id)
+      colors = @user_color.colors.split(':')
+      @initial_value1 = colors[0]
+      @initial_value2 = colors[1]
+      @initial_value3 = colors[2]
+      @initial_value4 = colors[3]
+    else
+      # 未登録の場合はデフォルト初期値をViewに渡す
+      @user_color = UserColor.new
+      @initial_value1 = "#ff0000"
+      @initial_value2 = "#0000ff"
+      @initial_value3 = "#00ff00"
+      @initial_value4 = "#ffff00"
+    end
+    # colorここまで
   end
 end
