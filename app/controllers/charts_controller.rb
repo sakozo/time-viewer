9a7e8117954327e4f9edcb2623106df5b4087d56
sakozo <youlianggit@gmail.com>
2020-08-31 20:26:53 +0900

    add: 日付の選択機能

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,33 +1,33 @@
 class ChartsController < ApplicationController
   def show
-    # スタック棒グラフ 1習慣 ここから
+    # 棒グラフ 1週間 ここから
     @all_select_time = OwnTime.all
 
-    base_day = Date.today
+    # 入力日を基準(base_day)に1週間前まで
+    base_day = date_params.blank? ? Date.today : date_params.to_date
 
-    if select_time_params != nil
-      select_time = OwnTime.find(select_time_params)
-      week_array = []
-      statistics_select_time_week = []
-      tmp_result_time = ResultTime.where(user_id: current_user.id).where(own_time_id: select_time)
-      (0..6).reverse_each do |i|
-        week_array << base_day - i
-        statistics_select_time_week << tmp_result_time.where(record_date: base_day - i).count/4.to_f
-      end
-      gon.label = week_array
-      gon.statistics_select_time_week = statistics_select_time_week
-      gon.title = select_time.name
-    else
-      gon.label = [1,2]
-      gon.statistics_select_time_week = [3,4]
-      gon.title = "睡眠時間にしたいのだが"
+    # デフォルトで睡眠時間を表示する
+    select_time_param = select_time_params.blank? ? select_time_param = 1 : select_time_params
+    select_time = OwnTime.find(select_time_param)
+    week_array = [] # 1週間の日付を格納
+    statistics_select_time_week = [] # own_timeのブロック数 ÷ 4 (per時間)を格納
+    tmp_result_time = ResultTime.where(user_id: current_user.id).where(own_time_id: select_time)
+    (0..6).reverse_each do |i|
+      week_array << base_day - i
+      statistics_select_time_week << tmp_result_time.where(record_date: base_day - i).count/4.to_f
     end
-    # スタック棒グラフ 1習慣 ここまで
-
-  end
+    gon.label = week_array
+    gon.statistics_select_time_week = statistics_select_time_week
+    gon.title = select_time.name
+    end
+    # 棒グラフ 1週間 ここまで
 
   private
   def select_time_params
     params[:select_time_0]
+  end
+
+  def date_params
+    params[:date]
   end
 end
