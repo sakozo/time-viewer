b937ad0a6f3a43ef8c7899efabfc4763a85d7864
sakozo <youlianggit@gmail.com>
2020-09-12 22:38:59 +0900

    fix: 取り組み中と完了のタスクをまとめる

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,27 +2,32 @@ class TasksController < ApplicationController
   def index
     tasks = OwnTime.where(user_id: current_user.id).where(task_flg: 1)
 
-    @doing_task_result_time_list = {}
-    tasks.where(done_task_flg: 0).each do |task|
+    @task_result_time_list = {}
+    # 逆ループで新しいものが上にくるようにする
+    tasks.reverse_each do |task|
       task_count = ResultTime.where(own_time_id: task.id).count / 4.to_f
-      @doing_task_result_time_list[task] = task_count
-    end
-
-    @done_task_result_time_list = {}
-    tasks.where(done_task_flg: 1).each do |task|
-      task_count = ResultTime.where(own_time_id: task.id).count / 4.to_f
-      @done_task_result_time_list[task] = task_count
+      @task_result_time_list[task] = task_count
     end
   end
 
   def update
     task = OwnTime.find(get_task_id)
     today = Date.today.to_time
-    # done_flgを1（完了）にする
-    if task.update(done_task_flg: 1, done_date: today)
-      redirect_to tasks_path
+
+    if task.done_task_flg == 0
+      # done_flgを1（完了）にする
+      if task.update(done_task_flg: 1, done_date: today)
+        redirect_to tasks_path
+      else
+        # TODO: 処理
+      end
     else
-      # TODO: 処理
+      # done_flgを0（取り組み中）にする
+      if task.update(done_task_flg: 0, done_date: nil)
+        redirect_to tasks_path
+      else
+        # TODO: 処理
+      end
     end
   end
 
