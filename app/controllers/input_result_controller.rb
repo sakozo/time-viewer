775a0cb356b7541e71395dde456d8134df4c216d
sakozo <youlianggit@gmail.com>
2020-10-04 13:20:26 +0900

    add:　フォローしていないユーザーの入力結果画面への遷移を制御

--- app/controllers/input_result_controller.rb
+++ app/controllers/input_result_controller.rb
@@ -1,5 +1,5 @@
 class InputResultController < ApplicationController
-
+  before_action :check_following_user?, only: [:show]
   def show
     @user = User.find(params[:id])
     tweets = Tweet.where(user_id: params[:id]).sort do |a,b| a[:record_date] <=> b[:record_date] end
@@ -17,4 +17,11 @@ class InputResultController < ApplicationController
 
   end
 
+  private
+  def check_following_user?
+    if current_user.check_following?(params[:id]) || (current_user.id == params[:id].to_i)
+    else
+      redirect_to root_path
+    end
+  end
 end
