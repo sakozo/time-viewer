2a77e3c346e53bd22a40cb406aafecfc633b9975
sakozo <youlianggit@gmail.com>
2020-10-04 11:14:45 +0900

    fix: 仮置き部分の修正

--- app/controllers/input_result_controller.rb
+++ app/controllers/input_result_controller.rb
@@ -2,7 +2,18 @@ class InputResultController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweets = Tweet.where(user_id: params[:id])
+    tweets = Tweet.where(user_id: params[:id]).sort do |a,b| a[:record_date] <=> b[:record_date] end
+    diaries = Diary.where(user_id: params[:id])
+
+    @input_results = []
+    tweets.each do |tweet|
+      tmp_diaries = diaries.where(record_date: tweet.record_date)
+      if tmp_diaries.present?
+        @input_results << [tweet, tmp_diaries]
+      else
+        @input_results << [tweet]
+      end
+    end
 
   end
 
