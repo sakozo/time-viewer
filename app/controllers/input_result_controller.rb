de45a297f40eb3512e8aeee040c74e598f29bd8e
sakozo <youlianggit@gmail.com>
2020-10-11 10:23:06 +0900

    add: 時間管理画面にスクロールバー追加

--- app/controllers/input_result_controller.rb
+++ app/controllers/input_result_controller.rb
@@ -2,22 +2,22 @@ class InputResultController < ApplicationController
   before_action :check_following_user?, only: [:show]
   def show
     @user = User.find(params[:id])
-    tweets = Tweet.where(user_id: params[:id]).sort do |a,b| a[:record_date] <=> b[:record_date] end
+    tweets = Tweet.where(user_id: params[:id]).sort { |a, b| a[:record_date] <=> b[:record_date] }
     diaries = Diary.where(user_id: params[:id])
 
     @input_results = []
     tweets.each do |tweet|
       tmp_diaries = diaries.where(record_date: tweet.record_date)
-      if tmp_diaries.present?
-        @input_results << [tweet, tmp_diaries]
-      else
-        @input_results << [tweet]
-      end
+      @input_results << if tmp_diaries.present?
+                          [tweet, tmp_diaries]
+                        else
+                          [tweet]
+                        end
     end
-
   end
 
   private
+
   def check_following_user?
     if current_user.check_following?(params[:id]) || (current_user.id == params[:id].to_i)
     else
